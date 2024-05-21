f027eab9e7af66e25f86dfd5c06c46fad7217d6c
CodingCashew <168357410+CodingCashew@users.noreply.github.com>
2024-05-21 10:26:50 -0500

    Debug

--- server/server.ts
+++ server/server.ts
@@ -8,7 +8,7 @@ require("dotenv").config();
 const sheetsController = require("./sheetsController");
 const emailController = require("./emailController");
 // const paymentController = require("./paymentController");
-
+console.log('in server ----------------------->');
 // parse incoming requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +19,7 @@ app.use(express.static(path.resolve(__dirname, "../src/assets/")));
 app.get("/", (req: Request, res: Response) => {
   res.status(200).sendFile(path.join(__dirname, "../public/index.html"));
 });
-// emailController.createMailingListTable()
+
 app.get(
   "/getSheets",
   sheetsController.getSheets,
