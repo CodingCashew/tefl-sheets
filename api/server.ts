0aad882a279ac3fa74f58b96dc79f26849c18f44
CodingCashew <168357410+CodingCashew@users.noreply.github.com>
2024-05-24 09:54:30 -0500

    debug

--- api/server.ts
+++ api/server.ts
@@ -16,13 +16,13 @@ console.log("port: --->", PORT);
 
 // // parse incoming requests
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // serve static and html
 app.use(express.static(path.resolve(__dirname, "../src/index.css")));
-app.use(express.static(path.resolve(__dirname, "../public/")));
-// app.use(express.static(path.resolve(__dirname, "../public/assets/")));
+// app.use(express.static(path.resolve(__dirname, "../public/")));
+app.use(express.static(path.resolve(__dirname, "../public/assets/")));
 app.get("/", (req: Request, res: Response) => {
 // app.get("/*", (req: Request, res: Response) => {
   res.status(200).sendFile(path.join(__dirname, ".index.html"));
